0df1c3978cfa613f48ef8bbc386e06b1bb208938
egor-achkasov <eaachkasov@gmail.com>
2026-05-08 20:15:47 +0000

    impl std::fmt::Debug for Event

--- src/lib/event.rs
+++ src/lib/event.rs
@@ -15,3 +15,21 @@ pub enum Event {
 
     Done,
 }
+
+impl std::fmt::Debug for Event {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Event::GetChannelStarted(url) => write!(f, "GetChannelStarted({})", url),
+            Event::GetChannelFailed(url, err) => write!(f, "GetChannelFailed({}, {})", url, err),
+            Event::GetChannelFinished(url) => write!(f, "GetChannelFinished({})", url),
+            Event::GetPostsStarted(url) => write!(f, "GetPostsStarted({})", url),
+            Event::GetPostsFailed(url, err) => write!(f, "GetPostsFailed({}, {})", url, err),
+            Event::GetPostsFinished(url) => write!(f, "GetPostsFinished({})", url),
+            Event::DownloadPostStarted(name) => write!(f, "DownloadPostStarted({})", name),
+            Event::DownloadPostFailed(name, err) => write!(f, "DownloadPostFailed({}, {})", name, err),
+            Event::DownloadPostSkipped(name) => write!(f, "DownloadPostSkipped({})", name),
+            Event::DownloadPostFinished(name) => write!(f, "DownloadPostFinished({})", name),
+            Event::Done => write!(f, "Done"),
+        }
+    }
+}
